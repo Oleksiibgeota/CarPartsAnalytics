e7f46f4b7c4839cee625339d24b9ea55158d7b1f
Oleksii <bgeota@gmail.com>
2018-04-03 16:19:15 +0300

    create method MySqlConnection

---
+++ src/main/java/com/carPatsAnalytics/dao/MySqlDBConnection.java
@@ -0,0 +1,38 @@
+package com.carPatsAnalytics.dao;
+
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.util.concurrent.atomic.AtomicLong;
+
+public class MySqlDBConnection {
+
+    private DataSource dataSource;
+    private static MySqlDBConnection instance;
+    private static final AtomicLong COUNT = new AtomicLong(0);
+
+
+    private MySqlDBConnection() {
+        System.out.println("new connection =  " + COUNT.incrementAndGet());
+        try {
+            dataSource = (DataSource) new InitialContext().lookup("java:comp/env/jdbc/Car_base");
+        } catch (NamingException e) {
+            throw new IllegalStateException("jdbc/Car_base" + " is missing in JNDI!", e);
+        }
+    }
+
+    public Connection getConnection() throws SQLException {
+        return dataSource.getConnection();
+    }
+
+    public static synchronized MySqlDBConnection getInstance() {
+        if (instance == null) {
+            instance = new MySqlDBConnection();
+        }
+        return instance;
+    }
+}
+
+
